aa781d360092ecfa879bf5e3d49d2d405a582db5
Samet ÇELİKBIÇAK <sametcelikbicak@outlook.com>
2018-03-24 15:07:41 +0300

    Initial Commit

--- Notification/ViewController.swift
+++ Notification/ViewController.swift
@@ -7,19 +7,63 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var bildirimBaslikTextField: UITextField!
+    @IBOutlet weak var bildirimDetayTextField: UITextField!
+    @IBOutlet weak var bildirimTarihDatePicker: UIDatePicker!
+    
+    var bildirimSayisi: Int = UserDefaults.standard.integer(forKey: "bildirimSayisi")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    func BildirimGonder (title titleText: String, body bodyText:String, date notificationDate:Date) {
+        let center = UNUserNotificationCenter.current()
+        let content = UNMutableNotificationContent()
+        content.title = titleText
+        content.body = bodyText
+        content.sound = UNNotificationSound.default()
+        content.badge = (self.bildirimSayisi + 1) as NSNumber
+        BildirimSayisiArttir(bildirimSayisi: self.bildirimSayisi + 1)
+        let trigerDate = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute,], from: notificationDate)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: trigerDate, repeats: false)
+        let identifier = UUID().uuidString
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        
+        center.add(request) { (error) in
+            if error != nil {
+                print("BildirimGonder Hata: \(String(describing: error))")
+            }
+        }
     }
-
-
+    
+    func BildirimSayisiArttir(bildirimSayisi: Int) {
+        if bildirimSayisi > -1 {
+            self.bildirimSayisi = bildirimSayisi
+            UserDefaults.standard.set(self.bildirimSayisi, forKey: "bildirimSayisi")
+        } else {
+            UIApplication.shared.applicationIconBadgeNumber = -1
+        }
+    }
+    
+    
+    @IBAction func bildirimGonder(_ sender: Any) {
+        BildirimGonder(title: bildirimBaslikTextField.text!, body: bildirimDetayTextField.text!, date: bildirimTarihDatePicker.date)
+    
+        let alert = UIAlertController(title: "Bildirim", message: "Bildirim Ayarlandı", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Tamam", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
 
