16c3df312364b37204739918dc2d25fe4522e09e
Samet ÇELİKBIÇAK <sametcelikbicak@outlook.com>
2018-03-24 13:27:05 +0300

    Initial Commit

---
+++ Notification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notification
+//
+//  Created by Samet ÇELİKBIÇAK on 24.03.2018.
+//  Copyright © 2018 Samet ÇELİKBIÇAK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
